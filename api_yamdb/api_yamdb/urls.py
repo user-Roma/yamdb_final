0e4713d7b29478adda0b04d71f387dea560430b6
Roman Iamov <yamov.ra@gmail.com>
2022-07-31 14:07:35 +0300

    add changes in main urls

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -4,10 +4,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
     path(
         'api/redoc/',
         TemplateView.as_view(template_name='api/redoc.html'),
         name='redoc'
     ),
+    path('api/', include('api.urls'))
 ]
